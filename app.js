26e3c200ceba839d8957e2bba4d7dd43fd6b195b
Ella Schwarz <ellaschwarzt@gmail.com>
2020-08-17 15:08:38 +0200

    Added body.parser and path modules

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ db.posts = new dataStore({
 db.comments = new dataStore({
     autoload: true
 })
+
+function post(title, content) {
+    db.posts.insert({title: title, content: content}, (err, newdoc) => {
+        console.log(newdoc)
+    })
+}
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+})
+
+app.listen(3000, () => {
+    console.log('listening to port 3000')
+})
